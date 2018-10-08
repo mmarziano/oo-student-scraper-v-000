52bd281b9e44a85bbc4eb381fbc50a88ebfec55f
Monica Marziano <mmarziano1@gmail.com>
2018-10-08 15:11:26 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -27,7 +27,13 @@ class Student
   end
 
   def add_student_attributes(attributes_hash)
-    attributes_hash.each do |attributes|
+    @twitter = attributes_hash[:twitter]
+    @linkedin = attributes_hash[:linkedin]
+    @github = attributes_hash[:github]
+    @blog = attributes_hash[:blog]
+    @profile_quote = attributes_hash[:profile_quote]
+    @bio = attributes_hash[:bio]
+    
       
   end
 
