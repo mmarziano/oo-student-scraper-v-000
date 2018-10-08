09549c23d1805860d934f247aeae32b113a302e9
Monica Marziano <mmarziano1@gmail.com>
2018-10-08 15:07:44 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -21,7 +21,9 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    Scraper.new
+    students_array.each do |student|
+      Student.new(student)
+    end
   end
 
   def add_student_attributes(attributes_hash)
